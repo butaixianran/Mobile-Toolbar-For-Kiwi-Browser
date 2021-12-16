9fcfb4977623a16e60b03bfdb4c8db272ad7b2a9
butaixianran <butaixianran@qq.com>
2021-12-17 07:25:34 +0800

    add undo button

--- src/background.js
+++ src/background.js
@@ -18,11 +18,30 @@ chrome.runtime.onMessage.addListener(
     } else if (request.ask === "forward") {
         chrome.tabs.goForward(sender.tab.id);
     } else if (request.ask === "close") {
+        //save tab's url for undo button
+        chrome.storage.local.set({undoUrl: sender.tab.url});
+
+        //close tab
         chrome.tabs.remove(sender.tab.id);
+
     } else if (request.ask === "reload") {
         chrome.tabs.reload(sender.tab.id);
     } else if (request.ask === "create") {
         chrome.tabs.create({});
+    } else if (request.ask === "undo") {
+        //get undo's url
+        chrome.storage.local.get(['undoUrl'], function(result) {
+          console.log('undoUrl is ' + result.undoUrl);
+
+          //check null
+          if(result && result.undoUrl) {
+            //open tab
+            chrome.tabs.create({ url: result.undoUrl });
+            //remove saved tab url
+            chrome.storage.local.set({undoUrl: ""});
+          }
+
+        });
     } else {
         console.log("unknow msg, do nothing");
     }
