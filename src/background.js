bef703a7198997ce1fc40a02a9c315e4ca1591d7
butaixianran <butaixianran@qq.com>
2026-04-15 08:30:33 +0800

    v0.3.1

--- src/background.js
+++ src/background.js
@@ -6,19 +6,20 @@ console.log("run mobile toolbar for kiwi background");
 //open last closed tab saved by this extension
 function openLastTab() {
     console.log("get last tab url saved by extension");
-    //get undo's url
-    chrome.storage.local.get(['undoUrl'], function(result) {
-        console.log('undoUrl is ' + result.undoUrl);
-
-        //check null
-        if(result && result.undoUrl) {
-            //open tab
-            chrome.tabs.create({ url: result.undoUrl });
-            //remove saved tab url
-            chrome.storage.local.set({undoUrl: ""});
+    //get undoList from storage, and get last url, then save undoList to storage
+    chrome.storage.local.get(['undoList'], function(result) {
+        let undoList = result.undoList || [];
+        if (undoList.length > 0) {
+            let lastUrl = undoList.pop();
+            console.log("open last tab with url: " + lastUrl);
+            chrome.tabs.create({url: lastUrl}); 
+            //save undoList to storage
+            chrome.storage.local.set({undoList: undoList});
+        } else {
+            console.log("no url in undoList, do nothing");
         }
-
     });
+
 }
 
 //get msg from content script
@@ -35,9 +36,16 @@ chrome.runtime.onMessage.addListener(
     } else if (request.ask === "forward") {
         chrome.tabs.goForward(sender.tab.id);
     } else if (request.ask === "close") {
-        //save tab's url for undo button
-        console.log("restoring tab with url: " + sender.tab.url);
-        chrome.storage.local.set({undoUrl: sender.tab.url});
+        //load undoList from storage, and add current url to undoList, then save undoList to storage
+        chrome.storage.local.get(['undoList'], function(result) {
+            let undoList = result.undoList || [];
+            undoList.push(sender.tab.url);
+            //undoList only save 30 url, if more than 30, remove the first one
+            if (undoList.length > 30) {
+                undoList.shift();
+            }
+            chrome.storage.local.set({undoList: undoList});
+        });
 
         //close tab
         chrome.tabs.remove(sender.tab.id);
@@ -47,36 +55,7 @@ chrome.runtime.onMessage.addListener(
     } else if (request.ask === "create") {
         chrome.tabs.create({url:"chrome://newtab"});
     } else if (request.ask === "undo") {
-
-        //get closed url by chrome.sessions.getRecentlyClosed
-        // get last session (maxResults: 1)
-        chrome.sessions.getRecentlyClosed({ maxResults: 1 }, (sessions) => {
-            
-            if (sessions && sessions.length > 0) {
-                console.log("recently closed sessions found, use chrome.sessions API to restore");
-                const lastSession = sessions[0];
-                const sessionId = lastSession.tab ? lastSession.tab.sessionId : lastSession.window.sessionId;
-                // restore and this session will be removed from list
-                chrome.sessions.restore(sessionId, (restored) => {
-                    if (restored.window){
-                        console.log("restroe a window");
-
-                    } else if (restored.tab) {
-                        console.log("restroe a tab");
-                    }
-                    else {
-                        console.log("nothing to restore, use saved url");
-                        openLastTab();
-                    }
-                });
-
-            } else {
-                console.log("no recently closed sessions found, use undoUrl saved by extension");
-                openLastTab();
-            }
-        });
-
-
+        openLastTab();
     } else {
         console.log("unknow msg, do nothing");
     }
